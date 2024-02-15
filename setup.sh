fc1a538d786178f285bac517d7fee46085d2be07
hirowassan <98885856+hirowassan@users.noreply.github.com>
2024-02-15 18:48:52 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ if check_java; then
     #サーバーをダウンロード
     curl -OJ https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.6/1.0.0/server/jar
     mv ./*.jar ./server.jar
-    
+
     #eulaを記述
     echo "eula=true" >> eula.txt
 
@@ -25,8 +25,7 @@ if check_java; then
 
     #modを導入(spigotでいうpluginを導入)
     mkdir mods && cd ./mods
-
-    #Carpetというmod(便利機能の詰め合わせ)のダウンロード
+        #Carpetというmod(便利機能の詰め合わせ)のダウンロード
     curl -OJ https://cdn.modrinth.com/data/TQTTVgYE/versions/K0Wj117C/fabric-carpet-1.20-1.4.112%2Bv230608.jar
 
     #FabricAPIという前提modをダウンロード
@@ -42,9 +41,12 @@ if check_java; then
     curl -OJ https://cdn.modrinth.com/data/NRjRiSSD/versions/5xvCCRjJ/memoryleakfix-fabric-1.17%2B-1.1.5.jar
 
     #バックアップmod
-    curl -JO https://hirowassan.com/mods/textile_backup-3.1.3-1.20.jar
+    curl -JO https://mediafilez.forgecdn.net/files/4749/914/textile_backup-3.1.2-1.20.jar
+　
+    echo "bash ./start.shでサーバーを起動できます。ワールドデータを移したいときは ~/minecraft-server/fabric-1.20.1 に移>動させてください。"
 
-    echo "bash ./start.shでサーバーを起動できます。ワールドデータを移したいときは ~/minecraft-server/fabric-1.20.1 に移動させてください。"
+    echo "server.propertiesのrate-limitを0にすると、パケット量によるキックが起こらなくなるのでcarpetを使う際は設定しておきましょう。"
 else
     echo "Javaがインストールされていません。インストールしてください。"
 fi
+                                     
